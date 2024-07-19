eb6501205595c815a66aba496d1fcbe65960ce3d
Joseph YM <josepym07@gmail.com>
2024-07-19 15:14:24 -0500

    Correcion de ubicacion

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,15 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart'; 
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
+class LoginScreen extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,80 +18,41 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 20),
             TextField(
               controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.email),
-              ),
+              decoration: InputDecoration(labelText: 'Email'),
             ),
-            SizedBox(height: 12),
             TextField(
               controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.lock),
-              ),
+              decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: isLoading
-                  ? null
-                  : () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      try {
-                        UserCredential userCredential =
-                            await FirebaseAuth.instance.signInWithEmailAndPassword(
-                          email: emailController.text,
-                          password: passwordController.text,
-                        );
-                        // Navega a la pantalla de chat después del login exitoso
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => ChatScreen()),
-                        );
-                      } on FirebaseAuthException catch (e) {
-                        // Maneja los errores de autenticación
-                        String errorMessage = 'Error desconocido';
-                        if (e.code == 'user-not-found') {
-                          errorMessage = 'Usuario no encontrado para ese correo electrónico.';
-                        } else if (e.code == 'wrong-password') {
-                          errorMessage = 'Contraseña incorrecta proporcionada.';
-                        } else if (e.code == 'invalid-email') {
-                          errorMessage = 'Formato de correo electrónico no válido.';
-                        }
-                        // Muestra un Snackbar de error al usuario
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(errorMessage),
-                            duration: Duration(seconds: 3),
-                          ),
-                        );
-                      } finally {
-                        setState(() {
-                          isLoading = false;
-                        });
-                      }
-                    },
-              child: isLoading
-                  ? SizedBox(
-                      width: 20,
-                      height: 20,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 3,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-                      ),
-                    )
-                  : Text('Login', style: TextStyle(fontSize: 16, fontFamily: 'Roboto', color: Colors.black)),
+              onPressed: () async {
+                try {
+                  UserCredential userCredential =
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => ChatScreen()), 
+                  );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'user-not-found') {
+                    print('No user found for that email.');
+                  } else if (e.code == 'wrong-password') {
+                    print('Wrong password provided.');
+                  }
+                }
+              },
+              child: Text('Login', style: TextStyle(fontSize: 16, fontFamily: 'Roboto', color: Colors.black)),
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 52, 159, 247)),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 15)),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 30, vertical: 15)),
                 textStyle: MaterialStateProperty.all<TextStyle>(
                   TextStyle(
                     fontSize: 16,
@@ -108,15 +63,25 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             SizedBox(height: 10),
-            TextButton(
+            ElevatedButton(
               onPressed: () {
-                // pantalla de registro al presionar el botón de registro
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => RegisterScreen()),
                 );
               },
-              child: Text('¿Nuevo? Regístrate aquí', style: TextStyle(fontSize: 16, fontFamily: 'Roboto', color: Color.fromARGB(255, 52, 159, 247))),
+              child: Text('Register', style: TextStyle(fontSize: 16, fontFamily: 'Roboto', color: Colors.black)),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 52, 159, 247)),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 30, vertical: 15)),
+                textStyle: MaterialStateProperty.all<TextStyle>(
+                  TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+              ),
             ),
           ],
         ),
@@ -125,98 +90,59 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-class RegisterScreen extends StatefulWidget {
-  @override
-  _RegisterScreenState createState() => _RegisterScreenState();
-}
-
-class _RegisterScreenState extends State<RegisterScreen> {
+class RegisterScreen extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Registro', style: TextStyle(fontFamily: 'Roboto', fontSize: 24)),
+        title: Text('Register', style: TextStyle(fontFamily: 'Roboto', fontSize: 24)),
         backgroundColor: Color.fromARGB(255, 52, 159, 247),
         centerTitle: true,
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 20),
             TextField(
               controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.email),
-              ),
+              decoration: InputDecoration(labelText: 'Email'),
             ),
-            SizedBox(height: 12),
             TextField(
               controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.lock),
-              ),
+              decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: isLoading
-                  ? null
-                  : () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      try {
-                        UserCredential userCredential =
-                            await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                          email: emailController.text,
-                          password: passwordController.text,
-                        );
-                        // Registro exitoso, puede navegar a otra pantalla o realizar acciones adicionales
-                      } on FirebaseAuthException catch (e) {
-                        // Maneja los errores de registro
-                        String errorMessage = 'Error desconocido';
-                        if (e.code == 'weak-password') {
-                          errorMessage = 'La contraseña proporcionada es demasiado débil.';
-                        } else if (e.code == 'email-already-in-use') {
-                          errorMessage = 'El correo electrónico proporcionado ya está en uso.';
-                        } else if (e.code == 'invalid-email') {
-                          errorMessage = 'Formato de correo electrónico no válido.';
-                        }
-                        // Muestra un Snackbar de error al usuario
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(errorMessage),
-                            duration: Duration(seconds: 3),
-                          ),
-                        );
-                      } finally {
-                        setState(() {
-                          isLoading = false;
-                        });
-                      }
-                    },
-              child: isLoading
-                  ? SizedBox(
-                      width: 20,
-                      height: 20,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 3,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-                      ),
-                    )
-                  : Text('Registro', style: TextStyle(fontSize: 16, fontFamily: 'Roboto', color: Colors.black)),
+              onPressed: () async {
+                try {
+                  UserCredential userCredential =
+                      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => ChatScreen()), 
+                  );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+              child: Text('Register', style: TextStyle(fontSize: 16, fontFamily: 'Roboto', color: Colors.black)),
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 52, 159, 247)),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 15)),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 30, vertical: 15)),
                 textStyle: MaterialStateProperty.all<TextStyle>(
                   TextStyle(
                     fontSize: 16,
